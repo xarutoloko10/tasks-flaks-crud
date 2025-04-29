603c8e2db5f35ed8510fa90c51cadc14569b3371
Minato-Xaruto <edson.autoequipe@gmail.com>
2025-04-29 13:20:57 -0300

    Adding tests & Updating create endpoint

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ def create_task():
     task_id_control += 1
     tasks.append(new_task)
     print(tasks)
-    return jsonify({"message": "Nova Tarefa criada com sucesso"})
+    return jsonify({"message": "Nova Tarefa criada com sucesso", "id": new_task.id})
 
 @app.route('/tasks', methods=['GET'])
 def get_tasks():
---
+++ tests.py
@@ -0,0 +1,63 @@
+import pytest
+import requests
+
+# CRUD
+BASE_URL = 'http://127.0.0.1:5000'
+tasks = []
+
+def test_create_task():
+    new_task_data = {
+        "title": "Nova tarefa",
+        "description": "Descrição da nova tarefa"
+    }
+    response = requests.post(f"{BASE_URL}/tasks", json=new_task_data)
+    assert response.status_code == 200
+    response_json = response.json()
+    assert "message" in response_json
+    assert "id" in response_json
+    tasks.append(response_json['id'])
+
+def test_get_tasks():
+    response = requests.get(f"{BASE_URL}/tasks")
+    response_json = response.json()
+    assert response.status_code == 200
+    assert "tasks" in response_json
+    assert "total_tasks" in response_json
+
+def  test_get_task():
+    if tasks:
+        task_id = tasks[0]
+        response = requests.get(f"{BASE_URL}/tasks/{task_id}")
+        response_json = response.json()
+        assert response.status_code == 200
+        assert task_id == response_json['id']
+
+def test_update_task():
+    if tasks:
+        task_id = tasks[0]
+        payload = {
+            "completed": True,
+            "description": "Nova descricção atualizada",
+            "title": "Titulo atualizado"
+        }
+        response = requests.put(f"{BASE_URL}/tasks/{task_id}", json=payload)
+        response.status_code == 200
+        response_json = response.json()
+        assert "message" in response_json
+
+        # Nova requisção a tarefa especifica
+        response = requests.get(f"{BASE_URL}/tasks/{task_id}")
+        response_json = response.json()
+        assert response.status_code == 200
+        assert response_json["title"] == payload["title"]
+        assert response_json["description"] == payload["description"]
+        assert response_json["completed"] == payload["completed"]
+
+def test_delete_task():
+    if tasks:
+        task_id = tasks[0]
+        response = requests.delete(f"{BASE_URL}/tasks/{task_id}")
+        response.status_code == 200
+
+        response = requests.get(f"{BASE_URL}/tasks/{task_id}")
+        assert response.status_code == 404
